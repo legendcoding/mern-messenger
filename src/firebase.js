ae26ba3b6d3e1313172d600f18e5d53adfe3c6f0
Yj-PC <yongjin91@gmail.com>
2020-10-08 17:25:14 +0800

    .gitignore update

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,15 @@
-import firebase from 'firebase'
+import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
-    // your Firebase credentials go here
-})
+  apiKey: "AIzaSyAePVL8dcHnGPS95RNhoya9FvwjSZBvE54",
+  authDomain: "mern-messenger-cd2af.firebaseapp.com",
+  databaseURL: "https://mern-messenger-cd2af.firebaseio.com",
+  projectId: "mern-messenger-cd2af",
+  storageBucket: "mern-messenger-cd2af.appspot.com",
+  messagingSenderId: "744682447814",
+  appId: "1:744682447814:web:201e9332e15ba7e4c408c7",
+});
 
-const db = firebaseApp.firestore()
+const db = firebaseApp.firestore();
 
-export default db
+export default db;
